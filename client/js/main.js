2795d2eab2af65f84d88ae1d77dc15bf67fb0344
Geraint White <geraintwhite@gmail.com>
2014-09-04 21:46:34 +0100

    Attempt at using physijs

--- client/js/main.js
+++ client/js/main.js
@@ -1,3 +1,7 @@
+Physijs.scripts.worker = '/js/physijs_worker.js';
+Physijs.scripts.ammo = '/js/ammo.js';
+
+
 var blocks = {
   ' ': {
     solid: false
@@ -60,7 +64,7 @@ function create_block(block_id, position) {
     var geometry = new THREE.BoxGeometry(1,1,2);
     var material = new THREE.MeshLambertMaterial({color: blocks[block_id].color});
 
-    var block = new THREE.Mesh(geometry, material);
+    var block = new Physijs.BoxMesh(geometry, material);
     block.position.x = position.x;
     block.position.y = position.y;
     block.position.z = -0.5;
@@ -122,7 +126,7 @@ function create_player(position) {
   var geometry = new THREE.SphereGeometry(0.5, 64, 64);
   var material = new THREE.MeshLambertMaterial({map: texture});
 
-  var player = new THREE.Mesh(geometry, material);
+  var player = new Physijs.SphereMesh(geometry, material);
   player.position.x = position.x;
   player.position.y = position.y;
   player.position.z = -0.5;
@@ -135,68 +139,6 @@ function create_player(position) {
   scene.add(player);
 
   return player;
-}
-
-
-function controls() {
-  if (Math.abs(player.position.x - old_x) >= 1) {
-    load_chunks();
-    old_x = player.position.x;
-  }
-
-  // Pyhsics and controls
-  if (keyboard.pressed('a')) {
-    player.velocity.x = -1;
-  }
-  if (keyboard.pressed('d')) {
-    player.velocity.x = 1;
-  }
-  if (keyboard.pressed('w')) {
-    player.velocity.y = 1;
-  }
-  if (keyboard.pressed('s')) {
-    player.velocity.x = 0;
-  }
-  if (player.velocity.x >= 0.1) {
-    player.velocity.x -= 0.1;
-  }
-  player.velocity.y -= 0.1;
-
-  // Collision detection
-  if (player.velocity.x && !solid_in_x(player.position, player.velocity)) {
-    player.position.x += player.velocity.x;
-  } else {
-    player.velocity.x = 0;
-  }
-
-  if (player.velocity.y && !solid_in_y(player.position, player.velocity)) {
-    player.position.y += player.velocity.y;
-  } else {
-    player.velocity.y = 0;
-  }
-
-  setTimeout(controls, 100);
-}
-
-
-function render() {
-  requestAnimationFrame(render);
-
-  // Camera, light positions
-  camera.position.x = player.position.x;
-  camera.position.y = player.position.y - (cam_dist * Math.tan(cam_rot));
-
-  light.position.x = player.position.x;
-  light.target.position.x = player.position.x;
-  light.target.position.y = player.position.y;
-  light.target.position.z = player.position.z;
-
-  shadow_light.position.x = player.position.x;
-  shadow_light.target.position.x = player.position.x;
-  shadow_light.target.position.y = player.position.y;
-  shadow_light.target.position.z = player.position.z;
-
-  renderer.render(scene, camera);
 }
 
 
@@ -213,6 +155,7 @@ function solid_in_x(position, velocity) {
     return screen_chunks[chunk][x][Math.floor(position.y)].block.solid;
   }
 }
+
 
 function solid_in_y(position, velocity) {
   var r = player.geometry.boundingSphere.radius;
@@ -234,53 +177,141 @@ function rad(deg) {
 }
 
 
-var socket = io('http://2.2.2.110:6001');
-var keyboard = new THREEx.KeyboardState();
-var scene = new THREE.Scene();
+function render() {
+  if (Math.abs(player.position.x - old_x) >= 1) {
+    load_chunks();
+    old_x = player.position.x;
+  }
 
-// Camera
-var width = window.innerWidth;
-var height = window.innerHeight;
-var FOV = 30;
-var cam_dist = 30;
-var cam_rot = -Math.PI / 12;
-var H_FOV = 2 * Math.atan(Math.tan(rad(FOV) / 2) * (width / height));
-var half_screen_blocks = Math.ceil(cam_dist * Math.tan(H_FOV / 2));
+  // Camera, light positions
+  camera.position.x = player.position.x;
+  camera.position.y = player.position.y - (cam_dist * Math.tan(cam_rot));
 
-var camera = new THREE.PerspectiveCamera(FOV, width / height, 1, 1000);
-camera.position.z = cam_dist;
-camera.rotation.x = cam_rot;
+  light.position.x = player.position.x;
+  light.target.position.x = player.position.x;
+  light.target.position.y = player.position.y;
+  light.target.position.z = player.position.z;
 
-// Light
-scene.add(new THREE.AmbientLight(0x202020));
+  shadow_light.position.x = player.position.x;
+  shadow_light.target.position.x = player.position.x;
+  shadow_light.target.position.y = player.position.y;
+  shadow_light.target.position.z = player.position.z;
 
-var light = new THREE.DirectionalLight(0x404040, 2);
-light.position.y = -10;
-light.position.z = cam_dist / 3;
-scene.add(light);
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+}
 
-var shadow_light = new THREE.SpotLight(0x404040, 5, 200);
-shadow_light.position.y = 30;
-shadow_light.position.z =  0;
-shadow_light.castShadow = true;
-shadow_light.shadowMapWidth = 1024;
-shadow_light.shadowMapHeight = 1024;
-shadow_light.shadowCameraNear = 5;
-shadow_light.shadowCameraFar = 50;
-shadow_light.shadowCameraFov = 30;
-scene.add(shadow_light);
 
-// Renderer
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(width, height);
-renderer.shadowMapEnabled = true;
-renderer.shadowMapSoft = true;
-document.body.appendChild(renderer.domElement);
+var socket,
+    keyboard,
+    scene,
+    width,
+    height,
+    FOV,
+    cam_dist,
+    cam_rot,
+    H_FOV,
+    half_screen_blocks,
+    camera,
+    light,
+    shadow_light,
+    renderer,
+    player,
+    old_x;
 
-// Player
-var player = create_player({x: 0, y: 15});
 
-// Start
-var old_x = 1;
-controls();
-render();
+function init() {
+  socket = io('http://2.2.2.110:6001');
+  keyboard = new THREEx.KeyboardState();
+
+  scene = new Physijs.Scene();
+  // scene.setGravity(new THREE.Vector3( 0, -30, 0 ));
+
+  scene.addEventListener('update', function() {
+
+    // Pyhsics and controls
+    if (keyboard.pressed('a')) {
+      player.velocity.x = -1;
+    }
+    if (keyboard.pressed('d')) {
+      player.velocity.x = 1;
+    }
+    if (keyboard.pressed('w')) {
+      player.velocity.y = 1;
+    }
+    if (keyboard.pressed('s')) {
+      player.velocity.x = 0;
+    }
+    if (player.velocity.x >= 0.1) {
+      player.velocity.x -= 0.1;
+    }
+    player.velocity.y -= 0.1;
+
+    player.position.x += player.velocity.x;
+    player.position.y += player.velocity.y;
+
+    // // Collision detection
+    // if (player.velocity.x && !solid_in_x(player.position, player.velocity)) {
+    //   player.position.x += player.velocity.x;
+    // } else {
+    //   player.velocity.x = 0;
+    // }
+
+    // if (player.velocity.y && !solid_in_y(player.position, player.velocity)) {
+    //   player.position.y += player.velocity.y;
+    // } else {
+    //   player.velocity.y = 0;
+    // }
+
+    scene.simulate();
+
+  });
+
+  // Camera
+  width = window.innerWidth;
+  height = window.innerHeight;
+  FOV = 30;
+  cam_dist = 30;
+  cam_rot = -Math.PI / 12;
+  H_FOV = 2 * Math.atan(Math.tan(rad(FOV) / 2) * (width / height));
+  half_screen_blocks = Math.ceil(cam_dist * Math.tan(H_FOV / 2));
+
+  camera = new THREE.PerspectiveCamera(FOV, width / height, 1, 1000);
+  camera.position.z = cam_dist;
+  camera.rotation.x = cam_rot;
+
+  // Light
+  scene.add(new THREE.AmbientLight(0x202020));
+
+  light = new THREE.DirectionalLight(0x404040, 2);
+  light.position.y = -10;
+  light.position.z = cam_dist / 3;
+  scene.add(light);
+
+  shadow_light = new THREE.SpotLight(0x404040, 5, 200);
+  shadow_light.position.y = 30;
+  shadow_light.position.z =  0;
+  shadow_light.castShadow = true;
+  shadow_light.shadowMapWidth = 1024;
+  shadow_light.shadowMapHeight = 1024;
+  shadow_light.shadowCameraNear = 5;
+  shadow_light.shadowCameraFar = 50;
+  shadow_light.shadowCameraFov = 30;
+  scene.add(shadow_light);
+
+  // Renderer
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(width, height);
+  renderer.shadowMapEnabled = true;
+  renderer.shadowMapSoft = true;
+  document.body.appendChild(renderer.domElement);
+
+  // Player
+  player = create_player({x: 0, y: 15});
+
+  // Start
+  old_x = 1;
+  render();
+}
+
+window.onload = init;
